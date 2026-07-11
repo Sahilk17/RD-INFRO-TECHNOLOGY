357bd40b71ee42a788aab337496d76e3dfbdfa3b
Sahilk17 <sahilkumar999088@gmail.com>
2026-07-11 17:53:36 +0530

    Create sketch.ino

---
+++ Task-4-I2C-OLED-Display/sketch.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 20);
+  display.println("Hello!");
+  display.display();
+}
+
+void loop() {
+}
