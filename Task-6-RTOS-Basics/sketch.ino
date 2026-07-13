807d522b3a8e2b04ead415e77cfe498f6c6fd75e
Sahilk17 <sahilkumar999088@gmail.com>
2026-07-13 18:08:55 +0530

    Add FreeRTOS tasks for serial output
    Implemented two FreeRTOS tasks that print messages to the serial monitor at different intervals.

---
+++ Task-6-RTOS-Basics/sketch.ino
@@ -0,0 +1,23 @@
+void Task1(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 1 Running");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void Task2(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 2 Running");
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  xTaskCreate(Task1, "Task1", 1000, NULL, 1, NULL);
+  xTaskCreate(Task2, "Task2", 1000, NULL, 1, NULL);
+}
+
+void loop() {
+}
